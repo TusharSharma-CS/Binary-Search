a8b75cdf804737d4beb7583f7c0e23891fabc5da
Tushar Sharma <tas2124@gmail.com>
2021-07-08 09:21:58 -0500

    Initial Commit

---
+++ BinarySearch.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class BinarySearch {
+	public static int binarySearch(int[] arr, int key) {
+		int l = 0;
+		int r = arr.length - 1;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			if (arr[mid] == key) {
+				return mid;
+			} else if (arr[mid] < key) {
+				l = mid + 1;
+			} else {
+				r = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		var scn = new Scanner(System.in);
+		System.out.println(
+				"First enter n in first line. In next n lines enter a number in each line. In last line enter target number");
+		System.out.println("enter size of array");
+		int n = scn.nextInt();
+		System.out.println("enter n integers");
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scn.nextInt();
+		}
+		Arrays.sort(arr);
+		scn.close();
+		System.out.println("enter target number");
+		int target = scn.nextInt();
+		System.out.println("the target is at : " + binarySearch(arr, target));
+	}
+}
---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+### Built With
+
+* [Java]()
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+* Compile and Run the application
+* Enter in how many ints will be in the array. Press Enter
+* Enter in, LINE BY LINE, an integer. 
+* Enter the number you are searching for after all ints have been entered.
+
+
+<!-- CONTACT -->
+## Contact
+
+Tushar Sharma or Troy - tas2124@gmail.com
+
+Project Link: [https://github.com/trrakee/notepadWebAPI](https://github.com/trrakee/notepadWebAPI)
